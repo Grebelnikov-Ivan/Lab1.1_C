76336091e71a984e4e93fb9d0f1c074d72e816fa
Grebelnikov_Ivan <grivmih@gmail.com>
2026-02-22 21:18:08 +0700

    add returns in del_same_ones
    *r_st = st, *r_len = j;

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include <string.h>
 
 void creat_set(char st[]);
-void del_same_ones(char st[], char ch, int start_i);
+void del_same_ones(char st[], char ch, int start_i, char**r_st, int*r_len);
 
 int main() {
     char st1[1000], st2[1000];
     scanf("%s", st1);
     printf("%s", st1);
     scanf("%s", st2);
-    del_same_ones(st2, 'a', 3);
-
+    char* r_st;
+    int r_len;
+    del_same_ones(st2, 'a', 3, &r_st, &r_len);
+    printf("%s", r_st);
+    printf("%d", r_len);
 
     return 0;
 
@@ -21,7 +24,7 @@ void creat_set(char st[]){
 
 }
 
-void del_same_ones(char st[], char ch, int start_i){
+void del_same_ones(char st[], char ch, int start_i, char**r_st, int*r_len){
     int i = start_i;
     int j = start_i;
 
@@ -33,7 +36,11 @@ void del_same_ones(char st[], char ch, int start_i){
         }
         i ++;
     }
-    for (int c = 0; c < j; c ++)
-        printf("%c", st[c]);
+    st[j] = '\0';
+    printf("%s", st);
+    /*for (int c = 0; c < j; c ++)
+        printf("%c", st[c]);*/
+    *r_st = st;
+    *r_len = j;
 }
 
