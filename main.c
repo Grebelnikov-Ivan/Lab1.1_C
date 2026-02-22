cba0f2aa3322090b4d88a7074cea590764b34cd8
Grebelnikov_Ivan <grivmih@gmail.com>
2026-02-22 20:33:47 +0700

    changed del_same_ones function

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
+void creat_set(char st[]);
+void del_same_ones(char st[], char ch, int start_i);
 
 int main() {
     char st1[1000], st2[1000];
     scanf("%s", st1);
     printf("%s", st1);
     scanf("%s", st2);
+    del_same_ones(st2, 'a', 3);
 
 
     return 0;
@@ -12,10 +17,23 @@ int main() {
 }
 
 
-void creat_set(char st){
+void creat_set(char st[]){
 
 }
 
-void del_same_ones(char st, char ch){
-    
+void del_same_ones(char st[], char ch, int start_i){
+    int i = start_i;
+    int j = start_i;
+
+    int len_st = strlen(st);
+    while (i < len_st){
+        if (st[i] != ch) {
+            st[j] = st[i];
+            j ++;
+        }
+        i ++;
+    }
+    for (int c = 0; c < j; c ++)
+        printf("%c", st[c]);
 }
+
