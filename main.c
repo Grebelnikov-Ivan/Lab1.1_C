77a22aa9931bc85aae8825a3477139587b78f0e0
Grebelnikov_Ivan <grivmih@gmail.com>
2026-02-22 18:00:14 +0700

    сreated functions
    creat_set, del_same_ones

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main() {
+    char st1[1000], st2[1000];
+    scanf("%s", st1);
+    printf("%s", st1);
+    scanf("%s", st2);
 
+
+    return 0;
+
+}
+
+
+void creat_set(char st){
+
+}
+
+void del_same_ones(char st, char ch){
+    
 }
